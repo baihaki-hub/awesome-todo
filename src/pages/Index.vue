67408e7aaa63ee49c89e85a7d8b154d73de7f145
baihaki-hub <baihaki.pru@gmail.com>
2019-10-10 11:04:35 +0700

    Initial Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-page padding>
+    <input v-model="message" @keyup="handleKeyup"
+	  @mouseleave="alertMessage"
+	/>
+	<button @click="clearMessage">Clear</button>
+    <h5 
+	    v-if="message.length" class="border-grey">{{ message }}</h5>
+	<h6 v-else>No Message Entered</h6>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    data() {
+	  return {
+		message: 'I love Vue.js'
+	  }
+	},
+	methods: {
+	  clearMessage() {
+		this.message = ''
+	  },
+	  handleKeyup(e) {
+		console.log(e)
+		if (e.keyCode == 27) {
+			this.clearMessage()
+		}
+		else if (e.keyCode == 13) {
+			this.alertMessage()
+		}
+	  },
+	  alertMessage() {
+		alert(this.message)
+	  }
+	}
+  }
+</script>
+
+<style>
+  .border-grey {
+	  border: 1px solid grey;
+  }
+</style>
